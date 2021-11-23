57709c69b149ad91b438f25e56bfca5a2d37e1ae
chan <90619921+chani017@users.noreply.github.com>
2021-11-23 12:36:28 +0900

    Add files via upload

---
+++ kurogagi_v1.2.3/popup/popup.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var links = document.getElementsByTagName("a");
+    for (var i = 0; i < links.length; i++) {
+        (function () {
+            var ln = links[i];
+            var location = ln.href;
+            ln.onclick = function () {
+                chrome.tabs.update({active: true, url: location});
+            };
+        })();
+    }
+
+document.getElementById("dpm").addEventListener("change",function() {
+    const loc = this.value;
+    if (loc) {
+         const varOpenNewTab = window.open(loc, '_blank');
+        if (varOpenNewTab) varOpenNewTab.focus();
+        }  
+    })
+});
